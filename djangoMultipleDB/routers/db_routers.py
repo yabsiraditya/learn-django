ccd43ead7c49eb5e26a79cfed28e01db693e064e
yabsiraditya <yabsir.aditya@gmail.com>
2024-10-03 20:48:49 +0700

    update learn-django multidatabase

--- djangoMultipleDB/routers/db_routers.py
+++ djangoMultipleDB/routers/db_routers.py
@@ -1,5 +1,4 @@
 # class oop
-
 class AuthRouter:
     
     route_app_labels = {"auth", "contenttypes", "admin", "sessions"}
@@ -53,4 +52,31 @@ class Blog:
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.route_app_labels:
             return db == "db_blog"
+        return None
+    
+class Kontak:
+    
+    route_app_labels = {"kontak"}
+
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label in self.route_app_labels:
+            return "db_kontak"
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label in self.route_app_labels:
+            return "db_kontak"
+        return None
+    
+    def allow_relation(self, obj1, obj2, **hints):
+        if (
+            obj1._meta.app_label in self.route_app_labels
+            or obj2._meta.app_label in self.route_app_labels
+        ):
+            return True
+        return None
+    
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label in self.route_app_labels:
+            return db == "db_kontak"
         return None
