28819820bcab0fafc3937abbdcb6f0c21f8ce1f5
yabsiraditya <yabsir.aditya@gmail.com>
2024-10-18 23:10:23 +0700

    update learn-django

--- djangoCRUD/pulangpergi/urls.py
+++ djangoCRUD/pulangpergi/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.addDestination , name='create'),
     path('delete/<slug:nameSlug>/', views.delete, name='delete'),
+    path('update/<slug:nameSlug>/', views.update, name='update'),
 ]
