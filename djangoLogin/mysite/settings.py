977e26bfb069f5ecca27711726e5e712badcea6a
yabsiraditya <yabsir.aditya@gmail.com>
2025-02-23 00:54:09 +0700

    update reset password

--- djangoLogin/mysite/settings.py
+++ djangoLogin/mysite/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR / 'emails'
 
 # Application definition
 
