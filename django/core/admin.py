44b1414818727b94a29b9f971aa8ee6563ed937e
yabsiraditya <yabsir.aditya@gmail.com>
2025-03-16 16:33:43 +0700

    update

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import WorkshopRepair, Rating, Sale
 
 # Register your models here.
+admin.site.register(WorkshopRepair)
+admin.site.register(Rating)
+admin.site.register(Sale)
